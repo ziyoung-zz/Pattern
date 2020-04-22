a35851edc478afadb6645a00658e040932894e70
ziyoung <cjy94911@gmail.com>
2020-04-22 14:56:30 +0900

    modify

--- Practice/Pro_12942.java
+++ Practice/Pro_12942.java
@@ -1,82 +1,46 @@
-package test;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
-public class study {
+public class Test {
 
    ////12942
    public static void main(String[] args){
       
-      int[][]matrix_sizes = {{4,10},{10,3},{3,4},{4,11}};  //270
+      int[][]matrix_sizes = {{5,3},{3,10},{10,6}};  //
       System.out.println(solution(matrix_sizes));
    }
 
  
    static int[][] m ;
+   static int[][] dparr;
+   public static int solution(int[][] matrix_sizes) {
+    	m = matrix_sizes;
+        dparr = new int[m.length][m.length];
+        return dp(0,m.length-1);
+   }
    
-    public static int solution(int[][] matrix_sizes) {
-    	
-    	 HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
-    	
-    	for(int i=0 ; i<matrix_sizes.length ; i++) {
-    		
-    		if(map.containsKey(matrix_sizes[i][0])) {
-    			map.get(matrix_sizes[i][0]).add(matrix_sizes[i][1]);
-    		}else {
-    			ArrayList<Integer> arr = new ArrayList<>();
-    			arr.add(matrix_sizes[i][1]);
-    			map.put(matrix_sizes[i][0], arr);
-    		}
-    	}
-    	
-    	int answer = -1;
-    	
-		for(int i=0 ; i<matrix_sizes.length ; i++) {
-			m = new int[matrix_sizes.length][2];
-	    	if(dfs(matrix_sizes[i][0],0,map)) {
-	    		int temp = dp(m.length,m,0,2);
-	    		if(answer==-1 || answer > temp) answer = temp;
-	    	}
-		}
+   private static int dp(int srt, int end){
+	   if(srt == end) return 0;
+	   if(dparr[srt][end] !=0 ) return dparr[srt][end];
+	   
+	   int min = Integer.MAX_VALUE;
+	   for(int i=srt ; i<end ; i++){
+		   min = Math.min(min, dp(srt,i) + dp(i+1,end) + m[srt][0] * m[i][1] * m[end][1]);
+	   }
+	   dparr[srt][end] = min;
+	   
+	   return min;
+   }
 
-        return answer;
-     }
-
+//처음 잘못생각한거   
+//    private static int dp(int n, int[][] m, int srt, int end){
+//       
+//        if(dparr[srt][end] != -1) return dparr[srt][end];
+//    	
+//       if(n == 2){
+//          return dparr[srt][end] = m[srt][0] * m[srt][1] * m[end][1];
+//       }else{
+//          
+//          return dparr[srt][end] = Math.min(dp(n-1,m,srt,end-1)+m[srt][0]*m[end][0]*m[end][1] , dp(n-1,m,srt+1,end)+ m[srt][0]*m[srt][1]*m[end][1]);          
+//       }      
+//    } 
     
-    private static boolean dfs(int key, int i, HashMap<Integer, ArrayList<Integer>> map) {
-    	
-       	if(i == m.length) return true;
-    	if(!map.containsKey(key)) return false;
-    	
-    	ArrayList<Integer> arr = map.get(key);
-    	
-    	for(int a=0 ; a < arr.size() ; a++) {
-    		int temp = arr.get(a);
-    		m[i][0] = key;
-    		m[i][1] = temp; 
-    		arr.remove(a);
-    		if(arr.size() == 0) {
-    			map.remove(key);
-    		}else {
-    			map.put(key,arr);
-    		}
-    		if(dfs(temp,i+1,map)) return true;
-    		
-    		arr.add(temp);
-    		map.put(key,arr);
-    	}
-    	
-    	return false;
-    }
-    
-    private static int dp(int n, int[][] m, int srt, int end){
-       
-       if(n == 2){
-          return m[srt][0] * m[srt][1] * m[end][1];
-       }else{
-          
-          return Math.min(dp(n-1,m,srt,end-1)+m[srt][0]*m[end][0]*m[end][1],dp(n-1,m,srt+1,end)+ m[srt][0]*m[srt][1]*m[end][1]);          
-       }      
-    }   
 }
